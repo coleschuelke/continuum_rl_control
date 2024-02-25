74e95ebf9db6fd1b954c375555ffc4a7f7e3404c
avalund <160542135+avalund@users.noreply.github.com>
2024-02-24 19:46:38 -0600

    Add files via upload

---
+++ Continuum_Calibration_22424.ino
@@ -0,0 +1,29 @@
+#include <Servo.h>
+
+//create servos
+Servo upServo; 
+Servo downServo;
+Servo rightServo;
+Servo leftServo; 
+
+//motor pins
+int upPos = 90;
+int downPos = 90;
+int rightPos = 90;
+int leftPos = 90;
+
+void setup() 
+{
+  upServo.attach(27);
+  downServo.attach(35);
+  rightServo.attach(23);
+  leftServo.attach(31);
+}
+
+void loop() 
+{
+    upServo.write(upPos);
+    downServo.write(downPos);
+    rightServo.write(rightPos); 
+    leftServo.write(leftPos);                        
+}
